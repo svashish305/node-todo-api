ef4903b6b37d6da11df05d3b7b1fef1dbc9c54c9
svashish305 <svashish305@gmail.com>
2018-08-25 11:36:19 +0530

    modified mongoose

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
-// process.env.MONGODB_URI = mongodb://admin:admin123@ds022228.mlab.com:22228/todo-app
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+// process.env.MONGOLAB_URI = mongodb://admin:admin123@ds022228.mlab.com:22228/todo-app
 module.exports = {mongoose};

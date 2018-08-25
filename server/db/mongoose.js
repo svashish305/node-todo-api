da5a8043ae83eda616fb47115619915893108029
svashish305 <svashish305@gmail.com>
2018-08-25 16:23:43 +0530

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI
-  || 'mongodb://admin:admin123@ds022228.mlab.com:22228/todo-app'
-   || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+  || 'mongodb://admin:admin123@ds022228.mlab.com:22228/todo-app',
+  { useNewUrlParser: true });
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 // process.env.MONGOLAB_URI = mongodb://admin:admin123@ds022228.mlab.com:22228/todo-app
 module.exports = {mongoose};
